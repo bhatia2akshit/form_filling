4f25581d5fdf8d20fa3310cddd659d389543be00
Akshit Bhatia <bhatia2akshit@gmail.com>
2025-04-02 18:06:51 +0200

    Update .gitignore with comprehensive patterns

--- frontend/src/app/services/config.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@angular/core';
-import { environment } from '../../environments/environments';
-
-@Injectable({providedIn: 'root'})
-export class ConfigService {
-  readonly apiUrl = environment.backendEndpoint;
-  readonly pdfPath = environment.pdfPath;
-  readonly jsonPath = environment.jsonPath;
-}
--- frontend/src/environments/environments.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-    production: false,
-    backendEndpoint: 'http://localhost:5000/api',
-    jsonPath: '/assets/json_objects/',
-    pdfPath: '/assets/input_files'
-
-  };
